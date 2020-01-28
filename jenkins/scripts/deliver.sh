aaaf3410bd85cfb0eefd60c067b10e0523d5a8ed
closecirlcesmtp@gmail.com <gmzolo@dearx.co.za>
2020-01-28 23:26:15 +0200

    Testing curl

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,7 +22,7 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-pwd
-ip a
 set -x
+cp target/${NAME}-${VERSION}.jar /var/jenkins_home/
+ls -l /var/jenkins_home/ 
 java -jar target/${NAME}-${VERSION}.jar
