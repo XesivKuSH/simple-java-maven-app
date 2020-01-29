ab920423a77aa6ef5ca7c3dccf298f47bf3c2f86
closecirlcesmtp@gmail.com <gmzolo@dearx.co.za>
2020-01-29 11:06:36 +0200

    Testing

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-git --version
+env
 java -jar target/${NAME}-${VERSION}.jar
