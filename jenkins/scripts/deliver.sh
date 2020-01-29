8b05c7e5a17ca2fbea1309777eeb41581f5fd234
closecirlcesmtp@gmail.com <gmzolo@dearx.co.za>
2020-01-29 10:57:48 +0200

    Testing

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,6 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-sudo cp target/${NAME}-${VERSION}.jar /var/jenkins_home/
-ls -l /var/jenkins_home/ 
+git --version
 java -jar target/${NAME}-${VERSION}.jar
